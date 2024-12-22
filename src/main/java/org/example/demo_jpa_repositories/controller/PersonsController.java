0d98a5a1dd67a6cd2104acc3fe3c40c94d8a37da
Dmitriy <160003751+Komanchero1@users.noreply.github.com>
2024-12-22 16:33:04 +0400

    Update PersonsController.java

--- src/main/java/org/example/demo_jpa_repositories/controller/PersonsController.java
+++ src/main/java/org/example/demo_jpa_repositories/controller/PersonsController.java
@@ -2,7 +2,6 @@ package org.example.demo_jpa_repositories.controller;
 
 import org.example.demo_jpa_repositories.entity.Persons;
 import org.example.demo_jpa_repositories.repository.PersonsRepository;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
+//класс обрабатывает HTTP-запросы начинающиеся с "/api/persons" и возвращает ответы
 @RestController
 @RequestMapping("/api/persons")
 public class PersonsController {
@@ -21,25 +21,38 @@ public class PersonsController {
         this.personsRepository = personsRepository;
     }
 
-    // Получить всех Persons по городу
-    @GetMapping("/city/{city}")//http://localhost:8080/api/persons/city/Moscow
-
-    public List<Persons> getPersonsByCity(@PathVariable String city) {
-        return personsRepository.getPersonsByCity(city);
+    // метод возвращает всех Persons по городу
+    @GetMapping("/city/{city}") // http://localhost:8080/api/persons/city/Moscow
+    public String getPersonsByCity(@PathVariable String city) {
+        List<Persons> persons = personsRepository.findByCityOfResidence(city);
+        // Используем StringBuilder для построения ответа
+        StringBuilder response = new StringBuilder();
+        for (Persons person : persons) {
+            response.append(person.toString()).append("\n");
+        }
+        return response.toString();
     }
 
-    // Получить Persons по возрасту, меньше указанного
+
+    // метод возвращает Persons по возрасту, меньше указанного
     @GetMapping("/age/less-than/{age}")//http://localhost:8080/api/persons/age/less-than/20
 
-    public List<Persons> getPersonsYoungerThan(@PathVariable int age) {
-        return personsRepository.findByAgeLessThanOrderByAgeAsc(age);
+    public String getPersonsYoungerThan(@PathVariable int age) {
+        List<Persons> persons = personsRepository.findByAgeLessThanOrderByAgeAsc(age);
+
+        StringBuilder response = new StringBuilder();
+
+        for (Persons person : persons) {
+            response.append(person.toString()).append("\n").append("\n");
+        }
+           return response.toString();
+        }
+
+    @GetMapping("/name/{name}/surname/{surname}") // http://localhost:8080/api/persons/name/Olga/surname/Karpova
+    public String getPersonByNameAndSurname(@PathVariable String name, @PathVariable String surname) {
+        Optional<Persons> person = personsRepository.findByNameAndSurname(name, surname);
+        return person.map(Persons::toString).orElse("Пользователь не найден"); // Возвращаем строку с информацией о человеке или сообщение об отсутствии
     }
 
-    // Получить Persons по имени и фамилии
-    @GetMapping("/name/{name}/surname/{surname}")//http://localhost:8080/api/persons/name/Olga/surname/Karpova
-    public ResponseEntity<Persons> getPersonByNameAndSurname(@PathVariable String name, @PathVariable String surname) {
-        Optional<Persons> person = personsRepository.findByNameAndSurname(name, surname);
-        return person.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
 }
 
